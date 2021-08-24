92a3e405ee85503ec9f1b67fe103e8776a632041
Konstantin_Maksimovich <Konstantin_Maksimovich@epam.com>
2021-08-24 23:35:41 +0400

    added read query secondaryKey

--- infrastructure/GenericTable.ts
+++ infrastructure/GenericTable.ts
@@ -11,6 +11,7 @@ export interface TableProps {
     readLambdaPath?: string,
     updateLambdaPath?: string,
     deleteLambdaPath?: string,
+    secondaryIndexes?: string[],
 }
 
 export class GenericTable {
@@ -38,6 +39,7 @@ export class GenericTable {
         this.createTable();
         this.createLambdas();
         this.grantTableRights();
+        this.addSecondaryIndexes();
     }
 
     private createTable() {
@@ -49,6 +51,20 @@ export class GenericTable {
             tableName: this.props.tableName,
         });
     }
+
+    private addSecondaryIndexes() {
+        if (this.props.secondaryIndexes) {
+            for (const secondaryIndex of this.props.secondaryIndexes) {
+                this.table.addGlobalSecondaryIndex({
+                    indexName: secondaryIndex,
+                    partitionKey: {
+                        name: secondaryIndex,
+                        type: AttributeType.STRING,
+                    },
+                });
+            }
+        }
+    };
 
     private createLambdas() {
         if (this.props.createLambdaPath) {
