2f2bb88e40f05451bfb335d84758ae822acf84cb
Konstantin_Maksimovich <Konstantin_Maksimovich@epam.com>
2021-08-24 18:17:21 +0400

    feat: added test connection s3Bucket

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/lib/aws-apigateway';
 import { GenericTable } from './GenericTable';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { PolicyStatement } from 'aws-cdk-lib/lib/aws-iam';
 
 export class SpaceStack extends Stack {
 
@@ -24,10 +25,14 @@ export class SpaceStack extends Stack {
             handler: 'hello.main',
         });
 
-        const helloNodejsFunction = new NodejsFunction(this, 'helloLambdaNodeJS', {
+        const helloLambdaNodeJS = new NodejsFunction(this, 'helloLambdaNodeJS', {
             entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
             handler: 'handler',
         });
+        const s3ListPolicy = new PolicyStatement();
+        s3ListPolicy.addActions('s3:ListAllMyBuckets');
+        s3ListPolicy.addResources('*');
+        helloLambdaNodeJS.addToRolePolicy(s3ListPolicy);
 
         const helloLambdaWebpack = new LambdaFunction(this, 'helloLambdaWebpack', {
             runtime: Runtime.NODEJS_14_X,
@@ -35,7 +40,7 @@ export class SpaceStack extends Stack {
             handler: 'nodeHelloLambda.handler',
         });
 
-        const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+        const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJS);
         const helloLambdaResource = this.api.root.addResource('hello');
         helloLambdaResource.addMethod('GET', helloLambdaIntegration);
     }
--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,6 +1,12 @@
+import { S3 } from 'aws-sdk';
+
+const s3Client = new S3();
+
 export const handler = async (event: any, context: any) => {
+    const buckets = await s3Client.listBuckets().promise();
+
     return {
         statusCode: 200,
-        body: 'Hello TS',
+        body: 'Buckets: ' + JSON.stringify(buckets.Buckets),
     };
 };
